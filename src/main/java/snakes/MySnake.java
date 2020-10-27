ff6a47bc8690a0acc1fd91c88e08ebee17d502e4
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-10-27 21:35:16 +0100

    Clean up code

--- src/main/java/snakes/MySnake.java
+++ src/main/java/snakes/MySnake.java
@@ -21,8 +21,6 @@ public class MySnake extends Snake {
 
     private int direction = RIGHT;
 
-    private Field[][] fields;
-
 
     public MySnake() {
         this.NAME = "YOUR_KUHL_SNAKE_NAME";         // set your favorite name
@@ -38,10 +36,9 @@ public class MySnake extends Snake {
     @Override
     public int think(BoardInfo board) {
         long start = System.currentTimeMillis();
-        fields = board.getFields();
 
         try {
-            direction = chosePath(board, createQueues(board)[0]);
+            // TODO
         } catch (Exception e) {
             direction = survive(board);
         }
@@ -58,7 +55,6 @@ public class MySnake extends Snake {
                 direction = (direction + 1) % 4;
             }
         }
-
         return direction;
     }
 
@@ -83,6 +79,8 @@ public class MySnake extends Snake {
         }
     }
     // TODO edge cases: can't reach any apple,
+
+
 
     private List<QueueItem> findPath(BoardInfo board, Field destination) {
         QueueItem[][] fields = assessField(board, destination);
@@ -157,167 +155,6 @@ public class MySnake extends Snake {
                     action.accept(fields[center.pos.getPosX()][center.pos.getPosY() + deltaY]);
                 }
             }
-        }
-    }
-
-    /*
-     * Function to create a Matrix of all fields and their distance of the way to the destination
-     */
-    QueueItem[][] addItem(QueueItem[][] fields, QueueItem item, int counter, BoardInfo board) {
-        fields[item.pos.getPosX()][item.pos.getPosY()] = item;
-
-        QueueItem newItem;
-
-        /*
-         * add item on the left
-         */
-        try {
-            newItem = new QueueItem(this .fields[item.pos.getPosX() - 1][item.pos.getPosY()], counter + 1);
-            if (newItem.pos.isFree()) {
-                if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()] == null) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                } else if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()].counter > newItem.counter) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {}
-
-        /*
-         * add item on the right
-         */
-        try {
-            newItem = new QueueItem(this.fields[item.pos.getPosX() + 1][item.pos.getPosY()], counter + 1);
-            if (newItem.pos.isFree()) {
-                if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()] == null) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                } else if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()].counter > newItem.counter) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {}
-
-        /*
-         * add item below
-         */
-        try {
-            newItem = new QueueItem(this.fields[item.pos.getPosX()][item.pos.getPosY() - 1], counter + 1);
-            if (newItem.pos.isFree()) {
-                if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()] == null) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                } else if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()].counter > newItem.counter) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {}
-
-        /*
-         * add item above
-         */
-        try {
-            newItem = new QueueItem(this.fields[item.pos.getPosX()][item.pos.getPosY() + 1], counter + 1);
-            if (newItem.pos.isFree()) {
-                if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()] == null) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                } else if (fields[newItem.pos.getPosX()][newItem.pos.getPosY()].counter > newItem.counter) {
-                    fields = addItem(fields, newItem, counter + 1, board);
-                }
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {}
-
-        return fields;
-    }
-
-
-    /*
-     * Algorithm to find a path to the nearest apple
-     * https://en.wikipedia.org/wiki/Pathfinding#:~:text=%20Algorithms%20used%20in%20pathfinding%20%201%20A,not%20restricted%20to%20move%20along%20the...%20More%20
-     */
-    private QueueItem[][][] createQueues(BoardInfo board) {
-        Field[][] fields = board.getFields();
-
-        Field[] applePos = board.getApples().toArray(new Field[0]);
-
-        QueueItem[][][] queues = new QueueItem[applePos.length][board.getSIZE_X()][board.getSIZE_Y()];
-
-        for (int i = 0; i < applePos.length; i++) {
-            queues[i] = new QueueItem[fields.length][fields[0].length];
-            QueueItem destination = new QueueItem(fields[applePos[i].getPosX()][applePos[i].getPosY()], 0);
-            queues[i][destination.pos.getPosX()][destination.pos.getPosY()] = destination;
-            queues[i] = addItem(queues[i], destination, 0, board);
-        }
-
-
-
-//        for (QueueItem[] column : queueMtrx) {
-//            for (QueueItem item: column) {
-//                if (item == null) {
-//                    System.out.printf("[ XX ]");
-//                } else {
-//                    System.out.printf("[ %s%d ]", (item.counter < 10) ? " " : "", item.counter);
-//                }
-//            }
-//            System.out.println();
-//        }
-
-
-        return queues;
-    }
-
-    /**
-     * chose the path based on the Matrix
-     *
-     * @param board     the whole board with every information necessary
-     * @param queueMtrx a Matrix of the fields filled with the needed amount of steps to the next apple
-     * @return direction in which the snake should move
-     */
-    private int chosePath(BoardInfo board, QueueItem[][] queueMtrx) {
-//        int direction;
-        int xMaxIndex = fields.length - 1;
-        int yMaxIndex = fields[0].length - 1;
-
-        Field headPos = board.getOwnHead();
-
-        int upCounter = queueMtrx.length * queueMtrx[0].length;
-        int downCounter = queueMtrx.length * queueMtrx[0].length;
-        int rightCounter = queueMtrx.length * queueMtrx[0].length;
-        int leftCounter = queueMtrx.length * queueMtrx[0].length;
-
-
-        if (headPos.getPosY() < yMaxIndex) {
-            if (fields[headPos.getPosX()][headPos.getPosY() + 1].isFree()) {
-                downCounter = queueMtrx[headPos.getPosX()][headPos.getPosY() + 1].counter;
-            }
-        }
-        if (headPos.getPosY() > 0) {
-            if (fields[headPos.getPosX()][headPos.getPosY() - 1].isFree()) {
-                upCounter = queueMtrx[headPos.getPosX()][headPos.getPosY() - 1].counter;
-            }
-        }
-        if (headPos.getPosX() < xMaxIndex) {
-            if (fields[headPos.getPosX() + 1][headPos.getPosY()].isFree()) {
-                rightCounter = queueMtrx[headPos.getPosX() + 1][headPos.getPosY()].counter;
-            }
-        }
-        if (headPos.getPosX() > 0) {
-            if (fields[headPos.getPosX() - 1][headPos.getPosY()].isFree()) {
-                leftCounter = queueMtrx[headPos.getPosX() - 1][headPos.getPosY()].counter;
-            }
-        }
-
-//        System.out.printf("x: %d, y: %d\n", headPos.getPosX(), headPos.getPosY());
-//        System.out.printf("upCounter:\t\t%d\ndownCounter:\t%d\nrightCounter:\t%d\nleftCounter:\t%d\n-----------------\n", upCounter, downCounter, rightCounter, leftCounter);
-
-
-        if (upCounter <= downCounter && upCounter <= rightCounter && upCounter <= leftCounter) {
-            return UP;
-        } else if (downCounter <= upCounter && downCounter <= rightCounter && downCounter <= leftCounter) {
-            return DOWN;
-        } else if (rightCounter <= upCounter && rightCounter <= downCounter && rightCounter <= leftCounter) {
-            return RIGHT;
-//        } else if (leftCounter <= upCounter && leftCounter <= downCounter && leftCounter <= rightCounter) {
-//            return LEFT;
-        } else {
-            return LEFT;
         }
     }
 }
